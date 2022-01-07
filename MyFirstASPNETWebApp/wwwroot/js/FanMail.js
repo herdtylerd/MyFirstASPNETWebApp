20052bd7d74778b69f863dcfcdf3f05aad5e7d63
herdtylerd <91562573+herdtylerd@users.noreply.github.com>
2022-01-07 16:36:30 -0700

    Add project files.

---
+++ MyFirstASPNETWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,13 @@
+﻿////$("#btnsend").addEventListener("click", function () {
+////    alert("From: " + $("#txtFrom").value +
+////        ", Subject: " + $("#txtSubject").value +
+////        ", Message: " + $("#txtMessage").value);
+////})
+
+$("#btnsend").click(function () {
+    alert("From: " + $("#txtFrom").val() +
+        ", Subject: " + $("#txtSubject").val() +
+        ", Message: " + $("#txtMessage").val());
+
+    $("#picGand").fadeOut("slow");
+})
